90bd89bf0577afb485d5baf4fc82738e148e7486
renan mello <mellorenan19@gmail.com>
2025-06-17 21:00:53 -0300

    feat: implement FavoriteService with storage

--- src/app/core/services/favorites.service.ts
+++ src/app/core/services/favorites.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
+  private _storage: Storage | null = null;
+  constructor(private storage: Storage   
+  ) {
+    this.init();
+   }
 
-  constructor() { }
+   async init() {
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+
+  async addFavorite(pokemon: any) {
+    const favorites = await this.getFavorites();
+    if (!favorites.some((fav: any) => fav.id === pokemon.id)) {
+      favorites.push(pokemon);
+      await this._storage?.set('favorites', favorites);
+    }
+  }
+
+  async removeFavorite(pokemonId: number) {
+    let favorites = await this.getFavorites();
+    favorites = favorites.filter((fav: any) => fav.id !== pokemonId);
+    await this._storage?.set('favorites', favorites);
+  }
+
+  async getFavorites(): Promise<any[]> {
+    const favorites = await this._storage?.get('favorites');
+    return favorites || [];
+  }
+
+  async isFavorite(pokemonId: number): Promise<boolean> {
+    const favorites = await this.getFavorites();
+    return favorites.some((fav: any) => fav.id === pokemonId);
+  }
+
 }
